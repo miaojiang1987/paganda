025b833b6f254ae03333486714f9ad1dcb97cb39
Miao Jiang <miaojiang1987@gmail.com>
2019-08-26 18:11:14 -0400

    Add a generic data_loader

--- data.py
+++ data.py
@@ -1,15 +1,18 @@
 import pickle as pk
 import os
 
-data = pk.load(open( "samples_211.pickle", "rb" ))
+#data = pk.load(open( "samples_211.pickle", "rb" ))
 
-print(type(data))
-print((data[0]))
+#print(type(data))
+#print((data[0]))
 
 def load_data(data_path,data):
-     data_files=os.listdir(data_path)
-     for i in range(len(data_files)):
-		data_temp=pk.load(open(data_files[i],'rb'))
-	    data.append(data_temp)
+    data_files=os.listdir(data_path)
+    for i in range(len(data_files)):
+        data_temp=pk.load(open(data_path+'/'+data_files[i],'rb'))
+        for temp in data_temp:
+            data.append(temp)
 
 data=[]
+load_data('data',data)
+print(len(data))
