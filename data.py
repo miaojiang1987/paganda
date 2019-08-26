affd660672ed7345fe5c9e9e868f15bcf729b12c
Miao Jiang <miaojiang1987@gmail.com>
2019-08-26 17:42:39 -0400

    change structure
    Change data structure

--- data.py
+++ data.py
@@ -1,6 +1,15 @@
 import pickle as pk
+import os
 
-data = pickle.load( open( "samples_211.pickle", "rb" ))))
+data = pk.load(open( "samples_211.pickle", "rb" ))
 
-print(data)
-print(len(data))
+print(type(data))
+print((data[0]))
+
+def load_data(data_path,data):
+     data_files=os.listdir(data_path)
+     for i in range(len(data_files)):
+		data_temp=pk.load(open(data_files[i],'rb'))
+	    data.append(data_temp)
+
+data=[]
